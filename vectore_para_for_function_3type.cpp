6a49823ebacfae54e52613b0988df874df87a7d4
tommykuo <chialiang2001@gmail.com>
2021-12-31 00:47:11 +0800

    Create vectore_para_for_function_3type.cpp

---
+++ vectore_para_for_function_3type.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void function1(std::vector<std::vector<int> > vec)
+{
+    cout<<"-----------------------------------------"<<endl;
+    //打印vec的地址
+    cout<<"function1.&vec:"<<&vec<<endl;
+    //打印vec[i]的地址（即第一层vector的地址）
+    cout<<"function1.&vec[i]:"<<endl;
+    for(int i=0;i<2;i++)
+        cout<<&vec[i]<<endl;
+    //打印vec的各元素地址
+    cout<<"function1.&vec[i][j]:"<<endl;
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<3;j++)
+            cout<<&vec[i][j]<<" ";
+        cout<<endl;
+    }
+    cout<<"---------------------------"<<endl;
+    //打印vec的各元素值
+    cout<<"function1.vec[i][j]:"<<endl;
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<3;j++)
+            cout<<vec[i][j]<<" ";
+        cout<<endl;
+    }
+}
+void function2(std::vector<std::vector<int> >& vec)
+{
+    cout<<"-----------------------------------------"<<endl;
+    //打印vec的地址
+    cout<<"function2.&vec:"<<&vec<<endl;
+    //打印vec[i]的地址（即第一层vector的地址）
+    cout<<"function2.&vec[i]:"<<endl;
+    for(int i=0;i<2;i++)
+        cout<<&vec[i]<<endl;
+    //打印vec的各元素地址
+    cout<<"function2.&vec[i][j]:"<<endl;
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<3;j++)
+            cout<<&vec[i][j]<<" ";
+        cout<<endl;
+    }
+    cout<<"---------------------------"<<endl;
+    //打印vec的各元素值
+    cout<<"function2.vec[i][j]:"<<endl;
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<3;j++)
+            cout<<vec[i][j]<<" ";
+        cout<<endl;
+    }
+
+}
+void function3(std::vector<std::vector<int> > *vec)
+{
+    cout<<"-----------------------------------------"<<endl;
+    //打印vec的地址
+    cout<<"function3.&vec:"<<vec<<endl;
+    //打印vec[i]的地址（即第一层vector的地址）
+    cout<<"function3.&vec[i]:"<<endl;
+    for(int i=0;i<2;i++)
+        cout<<&(*vec)[i]<<endl;
+    //打印vec的各元素地址
+    cout<<"function3.&vec[i][j]:"<<endl;
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<3;j++)
+            cout<<&(*vec)[i][j]<<" ";
+        cout<<endl;
+    }
+    cout<<"---------------------------"<<endl;
+    //打印vec的各元素值
+    cout<<"function3.vec[i][j]:"<<endl;
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<3;j++)
+            cout<<(*vec)[i][j]<<" ";
+        cout<<endl;
+    }
+}
+
+int main()
+{
+    vector<vector<int>> v(2, vector<int>(3,0));
+
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<3;j++)
+            v[i][j]=i+j;
+    }
+
+    cout<<"&v:"<<&v<<endl;
+    cout<<"&v[i]:"<<endl;
+    for(int i=0;i<2;i++)
+        cout<<&v[i]<<endl;
+
+    cout<<"&v[i][j]:"<<endl;
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<3;j++)
+            cout<<&v[i][j]<<" ";
+        cout<<endl;
+    }
+
+    cout<<"---------------------------"<<endl;
+    cout<<"v[i][j]:"<<endl;
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<3;j++)
+            cout<<v[i][j]<<" ";
+        cout<<endl;
+    }
+
+    function1(v);
+    //function2(v);
+    //function3(&v);
+
+    return 0;
+}
